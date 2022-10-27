f0477df52f883e958b685f9d4c967c2a6994f6de
zhaozhang <zhaozhang55@icloud.com>
2022-10-27 17:22:28 +0800

    Changes to be committed:

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
-from urllib.request import urlopen
+import requests
 
-myURL = urlopen("https://www.runoob.com/")
-# 读取一行内容
-print(myURL.readline())
-print("hello world")
+kw = {"tagId": 9, "memberName": "", "current": 1, "size": 10}
+
+url = "https://api.banksteel.com/gateway/banksteel-riskmanage-bcs/bcs/v2/tags/tagId/member-scores"
+response = requests.post(url, json=kw)
+# print(response.json)
+getParam = {
+    "originalIdListString": "1,2"
+    }
+getUrl = "https://api.banksteel.com/gateway/banksteel-finance-invoice/foreign/input/record/item"
+getResponse = requests.get(getUrl, params= getParam)
+print(getResponse.text)
